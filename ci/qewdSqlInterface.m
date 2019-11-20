dca79aba1732f568c582441a8ab5165a697f77a1
robtweed <rtweed@mgateway.com>
2019-11-20 11:40:57 +0000

    Build 0.1.6: Fully working mg-sql interface for QEWD-JSdb

--- ci/qewdSqlInterface.m
+++ ci/qewdSqlInterface.m
@@ -1,12 +1,44 @@
+ ; ----------------------------------------------------------------------------
+ ;| ewd-document-store: Persistent JavaScript Objects and Document Database  |
+ ;|                      using Global Storage                                |
+ ;|                                                                          |
+ ;| Copyright (c) 2016-19 M/Gateway Developments Ltd,                        |
+ ;| Redhill, Surrey UK.                                                      |
+ ;| All rights reserved.                                                     |
+ ;|                                                                          |
+ ;| http://www.mgateway.com                                                  |
+ ;| Email: rtweed@mgateway.com                                               |
+ ;|                                                                          |
+ ;|                                                                          |
+ ;| Licensed under the Apache License, Version 2.0 (the "License");          |
+ ;| you may not use this file except in compliance with the License.         |
+ ;| You may obtain a copy of the License at                                  |
+ ;|                                                                          |
+ ;|     http://www.apache.org/licenses/LICENSE-2.0                           |
+ ;|                                                                          |
+ ;| Unless required by applicable law or agreed to in writing, software      |
+ ;| distributed under the License is distributed on an "AS IS" BASIS,        |
+ ;| WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. |
+ ;| See the License for the specific language governing permissions and      |
+ ;|  limitations under the License.                                          |
+ ;----------------------------------------------------------------------------
+ ;
+ ; 20 November 2019
+ ;
  ; QEWD Interface to mgsql
-ping() ;
+ ;
+ping() ; // for testing
  s ^rob("ping")=$h
  QUIT "pong"
  ;
 sqlquery(sql) ;
+ ;
  n %zi,%zo
- s ^rob(1)=sql
+ ;
+ m %zi=^qewdTemp($j,"input")
+ s %zi("stmt")=1
  s ok=$$exec^%mgsql("",sql,.%zi,.%zo)
- s ^rob(2)=ok
+ s ^qewdTemp($j,"query")=sql
+ m ^qewdTemp($j,"output")=%zo
  QUIT ok
  ;
